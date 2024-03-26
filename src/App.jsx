f86c9c102954858215003c20eed451a585f4bb15
unknown <tauhidhossen04@gmail.com>
2024-03-26 11:45:03 +0600

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <h2>Welcome</h2>
+      
+      
+    </>
+  )
+}
+
+export default App
